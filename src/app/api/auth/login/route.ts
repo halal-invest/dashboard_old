68dd3a27c3c7694001358598100edc4cdd012f8d
Al Arafat Siddique <alarafatsiddique@gmail.com>
2023-10-20 19:28:50 +0600

    login route

---
+++ src/app/api/auth/login/route.ts
@@ -0,0 +1,70 @@
+import { NextResponse } from 'next/server';
+import prisma from '../../../../utils/connect';
+import { hash, compare } from 'bcrypt';
+import jwt from 'jsonwebtoken';
+import { serialize } from 'cookie';
+import { JWT_SECRET } from '../../../../utils/constants';
+const MAX_AGE = 60 * 60 * 24 *30;
+export const POST = async (request: Request) => {
+    const { email, password } = await request.json();
+    try {
+        const existUser = await prisma.user.findUnique({
+            where: { email },
+            select: {
+                username: true,
+                password: true,
+                email:true,
+                roles: {
+                    select: {
+                        permissions: {
+                            select: {
+                                title: true
+                            }
+                        }
+                    }
+                }
+            }
+        });
+
+        const uniquePermissionsSet = new Set();
+
+        existUser?.roles?.forEach((role) => {
+            role.permissions.forEach((permission) => {
+                uniquePermissionsSet.add(permission.title);
+            });
+        });
+        // const roles = existUser?.roles?.map((r)=>r.title);
+        const uniquePermissionsArray = Array.from(uniquePermissionsSet);
+
+        // console.log("Unique Permissions:", uniquePermissionsArray);
+
+        if (!existUser) {
+            return NextResponse.json({ message: 'No user found with this email.', status: false });
+        }
+        const passwordMatch = await compare(password, existUser.password);
+        if (passwordMatch) {
+            // const secret = process.env.JWT_SECRET;
+            const token = jwt.sign({ email, uniquePermissionsArray }, JWT_SECRET, { expiresIn: MAX_AGE });
+
+            const serialized = serialize('jwt', token, {
+                httpOnly: true,
+                secure: process.env.NODE_ENV === 'production',
+                sameSite: 'strict',
+                path: '/',
+                maxAge: MAX_AGE
+            });
+            const userInfo = {
+                name: existUser.username,
+                email:existUser.email,
+            }
+            return new Response(JSON.stringify({ user: userInfo, message: 'Authenticated', status: true }), {
+                headers: { 'Set-Cookie': serialized },
+                status: 200
+            });
+        } else {
+            return NextResponse.json({ message: 'Password Incorrect.', status: false });
+        }
+    } catch (error) {
+        return NextResponse.json({ message: error, status: 500, msg: 'catch error' });
+    }
+};
